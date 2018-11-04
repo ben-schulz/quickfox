5d1fa89fecfccf01ce6a1acd971f4d784889354e
ben-schulz <benjamin.john.schulz@gmail.com>
2018-11-04 11:43:03 -0600

    chore(project): remove obsolete.

--- util/pageinit.js
+++
@@ -1,7 +0,0 @@
-var textDestinationNode;
-var textDestinationNodeId;
-
-function pageInit(){
-    textDestinationNodeId = 'annotatedText';
-    textDestinationNode = document.getElementById(textDestinationNodeId);
-}
