942bcdcf880d55f080ee00dabcfa0ada5c9ae7f4
ben-schulz <benjamin.john.schulz@gmail.com>
2018-11-11 09:02:43 -0600

    test(lexeme): verify click highlight.

--- test/lexeme.js
+++ test/lexeme.js
@@ -1,4 +1,9 @@
 
-verify('successes are silent.', function() {
+verify('on click, highlight Lexeme.', function() {
 
+    var lex = new Lexeme('foo').render();
+
+    lex.click();
+
+    assert.isTrue( lex.classList.contains("relationForming") );
 });
