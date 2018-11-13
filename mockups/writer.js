1d280bad9ec0eab5642133506c7c05d5f37f4220
ben-schulz <benjamin.john.schulz@gmail.com>
2018-11-13 17:43:28 -0600

    feat(mockups): demo text buffer.
    Add working example of keyboard,
    buffer components used to apply
    lexing to a keyboard input.

---
+++ mockups/writer.js
@@ -0,0 +1,21 @@
+var buffer = new TextBuffer( document, "div" );
+var keyboard = new KeyboardInput( buffer.element );
+
+var canvas = document.getElementById( "textCanvasDemo" );
+
+console.info(canvas.firstChild);
+
+var onSpace = contents => {
+
+    return " " == contents.slice(-1);
+};
+
+var writeText = contents => {
+
+    canvas.appendChild(
+	new Lexeme( contents.join("") ).render() );
+};
+
+buffer.onFlush( writeText, onSpace );
+
+keyboard.bindInput( document );
