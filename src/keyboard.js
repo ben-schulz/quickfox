f63f9488e0138bc23f35f63f8ad86856ab167601
ben-schulz <benjamin.john.schulz@gmail.com>
2018-11-13 09:25:54 -0600

    feat(input): add 'keydown', 'keyup'.

--- src/keyboard.js
+++ src/keyboard.js
@@ -9,6 +9,25 @@ class KeyboardInput{
 	this.target.dispatchEvent( event );
     }
 
+    keydown( key ){
+
+	var detail = { "detail" : { "key": key } };
+
+	var event = new CustomEvent( "keydown", detail );
+
+	this.target.dispatchEvent( event );
+    }
+
+    keyup( key ){
+
+	var detail = { "detail" : { "key": key } };
+
+	var event = new CustomEvent( "keyup", detail );
+
+	this.target.dispatchEvent( event );
+    }
+
+
     constructor( target ){
 
 	this.target = target;
--- test/keyboard.js
+++ test/keyboard.js
@@ -16,3 +16,43 @@ verify("keypress registered", function(){
 
     assert.isTrue( registered );
 });
+
+
+verify("keydown registered", function(){
+
+    var boundElement = document.createElement( "div" );
+
+    var registered = false;
+    boundElement.addEventListener("keydown", function( event ){
+
+	if( 'x' == event.detail.key ){
+	    registered = true;
+	}
+    });
+
+    var keyboard = new KeyboardInput( boundElement );
+
+    keyboard.keydown( 'x' );
+
+    assert.isTrue( registered );
+});
+
+
+verify("keyup registered", function(){
+
+    var boundElement = document.createElement( "div" );
+
+    var registered = false;
+    boundElement.addEventListener("keyup", function( event ){
+
+	if( 'x' == event.detail.key ){
+	    registered = true;
+	}
+    });
+
+    var keyboard = new KeyboardInput( boundElement );
+
+    keyboard.keyup( 'x' );
+
+    assert.isTrue( registered );
+});
