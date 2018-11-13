8407bb69cd6a5f7c0c491b87a35c27986ce9a449
ben-schulz <benjamin.john.schulz@gmail.com>
2018-11-13 14:15:53 -0600

    feat(input): add input flushing.
    Add conditional 'flush' event
    to input buffer objects, so that
    grouping and filtering of keystrokes
    can precede other logic.

--- src/textBuffer.js
+++ src/textBuffer.js
@@ -1,13 +1,48 @@
 class TextBuffer{
 
+
+    clear(){
+
+	this.contents = [];
+    }
+
+    listenFor( condition ){
+
+	this._flushCondition = condition;
+    }
+
+    onFlush( action, condition ){
+
+	this.element.removeEventListener(
+	    "keypress", this.buffer );
+	
+	this.buffer = event => {
+
+	    this.contents.push( event.detail.key );
+
+	    if( !condition || condition( this.contents ) ){
+
+		action( this.contents );
+
+		this.clear();
+	    }
+	};
+
+	this.element.addEventListener(
+	    "keypress", this.buffer );
+    }
+
     constructor( document, type ){
 
 	this.element = document.createElement( type );
 	this.contents = [];
 
-	this.element.addEventListener("keypress", event => {
+	this.buffer = event => {
 
 	    this.contents.push( event.detail.key );
-	});
+	};
+
+	this.element.addEventListener(
+	    "keypress", this.buffer );
     }
 }
--- test/textBuffer.js
+++ test/textBuffer.js
@@ -8,3 +8,30 @@ verify("receives keystrokes from bound input", function(){
     assert.areEquivalent( "hello world",
 			  buffer.contents.join("") );
 });
+
+verify("flushes when condition met", function(){
+
+    var buffer = new TextBuffer( document, "div" );
+    var keyboard = new KeyboardInput( buffer.element );
+
+    var result = [];
+
+    var onSpace = function( contents ){
+
+	return " " == contents.slice(-1);
+    };
+
+    var pushWord = function( contents ){
+
+	var tokenLength = contents.length -1;
+	var token = contents.slice( 0, tokenLength ).join("");
+	result.push( token );
+    };
+
+    buffer.onFlush( pushWord, onSpace );
+
+    keyboard.typeKeys( "hello world " );
+
+    assert.areIdentical( "hello", result[0] );
+    assert.areIdentical( "world", result[1] );
+});
