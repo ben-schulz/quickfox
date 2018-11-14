63759acf0e4c8392b1330107bc0b01997df8ef94
ben-schulz <benjamin.john.schulz@gmail.com>
2018-11-13 21:53:54 -0600

    feat(mockups): handle backspace.

--- mockups/writer.js
+++ mockups/writer.js
@@ -10,6 +10,21 @@ var onSpace = contents => {
 
 var writeText = contents => {
 
+    if( "Backspace" === contents[0] ){
+
+	contents.splice( 0, 1 );
+    }
+
+    for( var ix = 1; ix < contents.length; ++ix ){
+
+	if( "Backspace" === contents[ix] ){
+
+	    contents.splice( ix, 1 );
+	    contents.splice( ix - 1, 1 );
+	}
+    }
+
+    console.info(contents);
     canvas.appendChild(
 	new Lexeme( contents.join("") ).render() );
 };
