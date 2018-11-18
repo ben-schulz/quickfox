a86806e3faf62ca9ae3efef453feceab2d07d269
ben-schulz <benjamin.john.schulz@gmail.com>
2018-11-18 11:09:22 -0600

    feat(mockups): demonstrate clear.
    show usage of the clear-on-escape
    feature for lexeme highlighting.

--- mockups/reader.js
+++ mockups/reader.js
@@ -22,6 +22,27 @@ fileSelector.addEventListener(
 		outputDiv.appendChild(
 		    new Lexeme( lexemes[ix] ).render() );
 	    }
+
+	    var onEscape = function( contents ){
+
+		return contents.slice(-1)[0].isEscape;
+	    };
+
+	    var raiseClear = function( contents ){
+
+		var event = new CustomEvent(
+		    "clearHighlights" );
+
+		outputDiv.childNodes.forEach(
+		    node => node.dispatchEvent( event ) )
+	    };
+
+	    var buffer = new TextBuffer( document, "div" );
+	    buffer.onFlush( raiseClear, onEscape );
+
+	    var keyboard = new KeyboardInput( buffer.element );
+	    keyboard.bindInput( document.body );
+
 	};
 
 	fileReader.readAsText( selectedFile );
